f93a0fdc417a1a4c019e2399fb43b1889072f4d6
danishkodemonkey <humberjini@hotmail.com>
2024-05-29 12:40:26 +0200

    BREAKING: Establish posts model for mongoose

---
+++ models/posts.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: { type: String, required: true, maxLength: 50 },
+    text: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Posts' }],
+    author: { type: Schema.Types.ObjectId, ref: 'Users' },
+    published: { type: Boolean, default: true },
+});
+
+PostSchema.virtual('url').get(function () {
+    return `/posts/${this._id}`;
+});
+
+module.exports = mongoose.model('Posts', UserSchema);
