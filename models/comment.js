2215b8548b75ae6df25a301822b0809eae5ca7bb
danishkodemonkey <humberjini@hotmail.com>
2024-05-31 14:37:25 +0200

    fix: added max length to comments

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-    text: { type: String, required: true },
+    text: { type: String, required: true, maxLength: 600 },
     timestamp: { type: Date, default: Date.now },
     post: { type: Schema.Types.ObjectId, ref: 'Posts' },
     author: { type: Schema.Types.ObjectId, ref: 'Users' },
@@ -13,4 +13,4 @@ CommentSchema.virtual('url').get(function () {
     return `/comments/${this._id}`;
 });
 
-module.exports = mongoose.model('Posts', UserSchema);
+module.exports = mongoose.model('Comments', CommentSchema);
