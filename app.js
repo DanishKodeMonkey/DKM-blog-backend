9b8e9028e978e2f48646e30fc4c193ba3199e3c6
danishkodemonkey <humberjini@hotmail.com>
2024-06-04 14:30:54 +0200

    Error handler fix

--- app.js
+++ app.js
@@ -23,12 +23,31 @@ main().catch(err => console.error(err));
 async function main() {
     await mongoose.connect(mongoDB);
 }
+// custom error handler for the API
+app.use((err, req, res, next) => {
+    console.error('custom error handler triggered');
+
+    if (err.status === 401) {
+        // unauthorized error
+        res.status(401).json({ error: 'Unauthorized access.' });
+    } else {
+        res.status(err.status || 500).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+});
+
 //middleware
 app.use(logger('combined'));
+console.log('logger initialised');
 app.use(cors());
+console.log('cors initialised');
 app.use(express.json());
+console.log('express json initialised');
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
+console.log('passport initialised');
 
 // set up routes
 app.use('/auth', authRouter);
@@ -37,16 +56,10 @@ app.use('/blog', blogRouter);
 
 // catch all route for undefined routes
 app.use((req, res, next) => {
+    console.error('custom catch all route triggered');
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
-// custom error handler for the API
-app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        status: 'error',
-        message: err.message,
-    });
 });
 
 // server setup
