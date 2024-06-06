e8493779c3bed241eef574a7dbf29df9f115ed04
danishkodemonkey <humberjini@hotmail.com>
2024-06-06 14:12:54 +0200

    feat: extra status logger for API boot

--- app.js
+++ app.js
@@ -14,13 +14,16 @@ const usersRouter = require('./routes/users');
 const blogRouter = require('./routes/blog');
 
 const app = express();
+console.warn('Starting express app..');
 // mongoDB setup and connect
 mongoose.set('strictQuery', false);
 const mongoDB = process.env.MONGO_URI;
 // attempt to start MongoDB operation
 
-main().catch(err => console.error(err));
+console.warn('Setting up mongoose connection...');
+main().catch((err) => console.error(err));
 async function main() {
+    console.log('Connecting to MongoDb...');
     await mongoose.connect(mongoDB);
 }
 // custom error handler for the API
@@ -60,6 +63,31 @@ app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
+});
+
+// custom error handler for the API
+app.use((err, req, res, next) => {
+    console.error('custom error handler triggered');
+    console.error('custom error handler triggered');
+    console.error('Request Method:', req.method);
+    console.error('Request URL:', req.url);
+    console.error('Request Headers:', req.headers);
+    console.error('Request Body:', req.body);
+    console.error('Query Parameters:', req.query);
+    console.error('Authenticated User:', req.user);
+    console.error('Error Stack Trace:', err.stack);
+
+    if (err.status === 401) {
+        // unauthorized error
+        res.status(401).json({ error: 'Unauthorized access.' });
+    } else {
+        console.error('Error 500 detected');
+
+        res.status(err.status || 500).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
 });
 
 // server setup
