fc818daebf4a9767dbf571f3b9c1871b3c945d51
danishkodemonkey <humberjini@hotmail.com>
2024-06-07 13:16:38 +0200

    fix: fix update user formatting and refactor to use save()

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,48 +73,73 @@ exports.createUser = [
             return next(err);
         } else {
             await user.save();
-            return res
-                .status(201)
-                .json({
-                    message: `User ${req.body.username} successfully saved`,
-                });
+            return res.status(201).json({
+                message: `User ${req.body.username} successfully saved`,
+            });
         }
     }),
 ];
 
 exports.editUser = [
     // validate new user data
-    body('username', 'Username must be specified')
+    body('username')
         .isLength({ min: 1, max: 30 })
         .withMessage('Keep username length between 1 and 30 characters')
         .escape(),
-    body('first_name', 'First name must be specified')
+    body('first_name')
         .trim()
         .isLength({ min: 1, max: 30 })
         .withMessage('Keep first name length between 1 and 30 characters')
         .escape(),
-    body('last_name', 'Last name must be specified')
+    body('last_name')
         .trim()
         .isLength({ min: 1, max: 30 })
         .withMessage('Keep last names between 1 and 30 characters')
         .escape(),
-    body('email', 'Email must be specified')
+    body('email')
         .trim()
         .isEmail()
         .withMessage('This email adress is invalid')
         .escape(),
-    body('password', 'A password is required')
-        .trim()
-        .isLength({ min: 4, max: 9999 })
-        .withMessage(
-            'Please ensure passwords are between 4 and 9999 characters'
-        ),
+    body('password').optional().trim(),
 
     // Process request after validation and sanitization
     asyncHandler(async (req, res, next) => {
-        // extract validatio nerrors from request
+        console.warn('editUser method triggered... validating data');
+        console.log('extracting validation errors from request...');
+        console.log('Received request body', req.body);
+        // extract validation nerrors from request
         const errors = validationResult(req);
 
+        if (!errors.isEmpty()) {
+            console.error('An error has occured', errors.array());
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        console.log('looking up user...', req.params.userId);
+        try {
+            const user = await Users.findById(req.params.userId);
+            if (!user) {
+                console.error('Error, user not found!');
+                return res.status(404).json({ message: 'User not found' });
+            }
+            console.log('User found, updating data...');
+            user.set({
+                username: req.body.username,
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                password: req.body.password,
+            });
+            const updatedUser = await user.save();
+
+            console.log('User updated sucessfully!! Operation complete!');
+            res.json(updatedUser);
+        } catch (error) {
+            next(error);
+        }
+        /* 
+        console.log('creating new user object');
         // create new user object with verified data
         const user = new Users({
             username: req.body.username,
@@ -123,8 +148,9 @@ exports.editUser = [
             email: req.body.email,
             password: req.body.password,
             // remember _id for user update on DB
-            _id: req.params.id,
+            _id: req.params.userId,
         });
+        console.log('user object created', user);
         if (!errors.isEmpty()) {
             console.error(`Something went wrong, user data: ${user}`);
             console.error(`Errors: ${errors}`);
@@ -132,15 +158,19 @@ exports.editUser = [
             err.status = 400;
             return next(err);
         } else {
+            console.log('Trying to update user', user, req.params.userId);
             const updatedUser = await Users.findByIdAndUpdate(
-                req.params.id,
+                req.params.userId,
                 user,
-                {}
+                { new: true, runValidators: true }
             );
-            res.send(
-                `User ${req.body.username} was updated successfully! Updated user: ${user}`
-            );
-        }
+            if (!updatedUser) {
+                console.error('404, User not found');
+                return res.status(404).send('User not found');
+            }
+            console.log('Operation successful!');
+            res.json(updatedUser);
+        } */
     }),
 ];
 
