729d03d6002123be01c76e602efb8ef83fb316eb
danishkodemonkey <humberjini@hotmail.com>
2024-10-04 16:09:14 +0200

    Refactor mongoose functiosn related to posts to prismaa queries

--- db/prismaQueries.js
+++ db/prismaQueries.js
@@ -1,4 +1,5 @@
 const prisma = require('./prismaClient');
+
 const userQueries = {
     createUser: async ({
         username,
@@ -72,6 +73,123 @@ const userQueries = {
         } catch (err) {
             console.error('Error fetching user by id: ', err);
             throw new Error(' Error fetching user by id');
+        }
+    },
+};
+
+const postQueries = {
+    getUserIdByUsername: async (username) => {
+        try {
+            const user = await prisma.user.findUnique({
+                where: { username: username },
+                select: { id: true },
+            });
+            return user ? user.id : null;
+        } catch (err) {
+            console.log('Error fetching userId', err);
+            throw new Error('Error fetching userId');
+        }
+    },
+    listPosts: async () => {
+        try {
+            const posts = await prisma.post.findMany({
+                include: {
+                    author: {
+                        select: {
+                            username: true,
+                        },
+                    },
+                    comments: {
+                        select: {
+                            text: true,
+                            timestamp: true,
+                            author: {
+                                select: {
+                                    username: true,
+                                },
+                            },
+                        },
+                    },
+                },
+                orderBy: {
+                    timestamp: 'desc',
+                },
+            });
+            return posts;
+        } catch (err) {
+            console.error('Error fetching all posts', err);
+            throw new Error('Error fetching all posts');
+        }
+    },
+    getPost: async (postId) => {
+        try {
+            const post = prisma.post.findUnique({
+                where: { id: parseInt(postId) },
+                include: {
+                    author: {
+                        select: {
+                            username: true,
+                        },
+                    },
+                    comments: {
+                        select: {
+                            text: true,
+                            timestamp: true,
+                            author: {
+                                select: {
+                                    username: true,
+                                },
+                            },
+                        },
+                    },
+                },
+            });
+            return post;
+        } catch (err) {
+            console.error('Error fetching post: ', err);
+            throw new Error('Error fetching post');
+        }
+    },
+    createPost: async ({ title, text, authorId }) => {
+        try {
+            const post = await prisma.post.create({
+                data: {
+                    title,
+                    text,
+                    authorId,
+                },
+            });
+            return post;
+        } catch (err) {
+            console.error('Error creating post: ', err);
+            throw new Error('Error creating post');
+        }
+    },
+    editPost: async ({ postId, data }) => {
+        try {
+            const updatedPost = await prisma.post.update({
+                where: { id: postId },
+                data: {
+                    title: data.title,
+                    text: data.text,
+                    author: data.authorId,
+                },
+            });
+            return updatedPost;
+        } catch (err) {
+            console.error('Error updating post: ', err);
+            throw new Error('Error updating post');
+        }
+    },
+    deletePost: async (postId) => {
+        try {
+            const deletedPost = await prisma.post.delete({
+                where: { id: postId },
+            });
+            return deletedPost;
+        } catch (err) {
+            console.error('Error deleting post: ', err);
+            throw new Error('Error deleting post.');
         }
     },
 };
