0fe69a966b5ded2f17f3a08c88f0343d5d0203d4
danishkodemonkey <humberjini@hotmail.com>
2024-10-04 15:36:20 +0200

    Implement refactored prisma queries

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
 const asyncHandler = require('express-async-handler');
-const Users = require('../models/user');
+const bcrypt = require('bcryptjs');
+const userQueries = require('../db/prismaQueries');
 const { body, validationResult } = require('express-validator');
 
 exports.listUsers = asyncHandler(async (req, res, next) => {
-    const allUsers = await Users.find().sort({ username: 1 }).exec();
+    const allUsers = await userQueries.listUsers();
     if (!allUsers.length) {
         return res.status(404).send('No users found');
     } else {
@@ -12,16 +13,7 @@ exports.listUsers = asyncHandler(async (req, res, next) => {
 });
 
 exports.getUser = asyncHandler(async (req, res, next) => {
-    const user = await Users.findById(req.params.userId)
-        .populate({
-            path: 'posts',
-            select: 'title timestamp',
-        })
-        .populate({
-            path: 'comments',
-            select: 'text timestamp',
-        })
-        .exec();
+    const user = await userQueries.getUser(req.params.userId);
 
     if (!user) {
         // no results
@@ -66,14 +58,19 @@ exports.createUser = [
         // extract validatio nerrors from request
         const errors = validationResult(req);
 
+        // bcrypt for password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         // create new user object with verified data
-        const user = new Users({
+        const newUser = await userQueries.createUser({
             username: req.body.username,
             first_name: req.body.first_name,
             last_name: req.body.last_name,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
+            membership: req.body.membership || 'User', //default membership is User
         });
+
         if (!errors.isEmpty()) {
             console.error(`Something went wrong, user data: ${user}`);
             console.error(`Errors: ${errors}`);
@@ -81,7 +78,6 @@ exports.createUser = [
             err.status = 400;
             return next(err);
         } else {
-            await user.save();
             return res.status(201).json({
                 message: `User ${req.body.username} successfully saved`,
             });
@@ -127,20 +123,16 @@ exports.editUser = [
 
         console.log('looking up user...', req.params.userId);
         try {
-            const user = await Users.findById(req.params.userId);
+            const user = await userQueries.getUser(req.params.userId);
             if (!user) {
                 console.error('Error, user not found!');
                 return res.status(404).json({ message: 'User not found' });
             }
-            console.log('User found, updating data...');
-            user.set({
-                username: req.body.username,
-                first_name: req.body.first_name,
-                last_name: req.body.last_name,
-                email: req.body.email,
-                password: req.body.password,
-            });
-            const updatedUser = await user.save();
+            console.log('User found, updating ...');
+            const updatedUser = await userQueries.editUser(
+                req.params.userId,
+                req.body
+            );
 
             console.log('User updated sucessfully!! Operation complete!');
             res.json(updatedUser);
@@ -151,15 +143,9 @@ exports.editUser = [
 ];
 
 exports.deleteUser = asyncHandler(async (req, res, next) => {
-    // TODO
-    // Make sure to delete any post and comments by user before getting here.
-    // Note: If posts does the same, maybe the process can be somewhat automated?
-    // Thought: Trigger deleteUser triggers all posts deletePosts triggers all post comments deleteComments.
-    // Make sure this is done asynchronously.
-
     const userId = req.params.userId;
 
-    const user = await Users.findById(userId);
+    const user = await userQueries.getUser(userId);
 
     if (!user) {
         return res.sendStatus(404, 'User not found');
@@ -167,7 +153,7 @@ exports.deleteUser = asyncHandler(async (req, res, next) => {
     if (req.user.id !== userId && req.user.membership !== 'Author') {
         return res.sendStatus(403, 'Unauthorized user deletion');
     }
-
-    await Users.findByIdAndDelete(userId);
+    console.log('Goodbye user: ', user.username);
+    await userQueries.deleteUser(userId);
     return res.send('User successfully deleted, goodbye.');
 });
